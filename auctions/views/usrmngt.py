2e4f000f4d80762086020e95dc1ac5d0673321d2
lcarland <arlandlc@gmail.com>
2023-10-09 06:32:46 -0700

    edit createlisting

--- auctions/views/usrmngt.py
+++ auctions/views/usrmngt.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render 
 
+import re
+
 
 def createlisting(request):
     if request.method == 'POST':
-        pass
-        #TODO 
+        title = request.POST["title"]
+        description = request.POST["description"]
+        tags = request.POST["tags"] # type str -> list
+        imgurl = request.POST["imgurl"]
+        startbid: float = request.POST["startbid"]
+
+        tags = tags.split(',')
+
+        for i, val in enumerate(tags):
+            tags[i] = val.strip()
+            if not val:
+                del tags[i]
+            
+        #TODO
 
     return render(request, 'auctions/createlisting.html', {
         'message' : ''
